2a112f679faed81ee7d5ef5f83ca6bff42e5be46
Sinrock <mruhaak@gmail.com>
2021-02-07 04:19:39 -0700

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,16 @@
 class StudentsController < ApplicationController
+	before_action :select_student, only: :show
+
   def index
     @students = Student.all
+  end
+
+  def show
+  end
+
+private
+
+  def select_student
+  	@students = Student.find(params[:id])
   end
 end
